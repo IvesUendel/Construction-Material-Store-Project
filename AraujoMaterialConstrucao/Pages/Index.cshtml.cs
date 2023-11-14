5fcbe7a30ca492449a61da315385cfa00c7421cc
Uendel Ives de Araujo <uendel.profissional@gmail.com>
2023-11-14 20:05:32 -0300

    Ajuste de detalhes no código do bootstrap dentro do HTML para melhoramento do card

--- AraujoMaterialConstrucao/Pages/Index.cshtml.cs
+++ AraujoMaterialConstrucao/Pages/Index.cshtml.cs
@@ -6,7 +6,7 @@ namespace AraujoMaterialConstrucao.Pages;
 
 public class IndexModel : PageModel
 {
-    public IList<Tile> TilesList { get; set; }
+    public IList<Tile> TilesList { get; private set; }
     public void OnGet()
     {
         TilesList = new List<Tile>()
@@ -16,7 +16,7 @@ public class IndexModel : PageModel
                 TileId = 1,
                 Name = "Archaic Tile",
                 Description = "Relive the past with our ancient tile model. Intricate patterns and a rich palette bring historical elegance to any space.",
-                ImageUri = "~/images/piso-1.jpg",
+                ImageUri = "/images/piso-1.jpg",
                 RegistrationDate = DateTime.Now,
                 ExpressDelivery = true,
                 Price = 14.99,
@@ -26,7 +26,7 @@ public class IndexModel : PageModel
                 TileId = 2,
                 Name = "Brick Tile",
                 Description = "Discover timeless charm with our brick-patterned tile. A blend of rusticity and elegance to transform your spaces.",
-                ImageUri = "~/images/piso-2.jpg",
+                ImageUri = "/images/piso-2.jpg",
                 RegistrationDate = DateTime.Now,
                 ExpressDelivery = true,
                 Price = 23.99,
@@ -36,7 +36,7 @@ public class IndexModel : PageModel
                 TileId = 3,
                 Name = "White Brick Tile",
                 Description = "Elegance in every detail. The white brick-patterned tile offers sophistication and brightness, transforming your spaces with a modern touch.",
-                ImageUri = "~/images/piso-3.jpg",
+                ImageUri = "/images/piso-3.jpg",
                 RegistrationDate = DateTime.Now,
                 ExpressDelivery = true,
                 Price = 27.99,
@@ -46,7 +46,7 @@ public class IndexModel : PageModel
                 TileId = 4,
                 Name = "Black and White Tile",
                 Description = "Modern in black and white. The tile features a bold design, blending contemporary elegance with white details for a distinctive touch.",
-                ImageUri = "~/images/piso-3.jpg",
+                ImageUri = "/images/piso-4.jpg",
                 RegistrationDate = DateTime.Now,
                 ExpressDelivery = true,
                 Price = 19.99,
