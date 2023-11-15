93e5a2a638e4c0127bb16559f06ee28ceafd31ee
Uendel Ives de Araujo <uendel.profissional@gmail.com>
2023-11-15 20:22:39 -0300

    Added the create page, implementation and improvement of HTML and inclusion of javascript for instant data validation on the client side

--- AraujoMaterialConstrucao/Models/Tile.cs
+++ AraujoMaterialConstrucao/Models/Tile.cs
@@ -5,23 +5,35 @@ namespace AraujoMaterialConstrucao.Models
     public class Tile
     {  
         public int TileId { get; set; }
+
+        [Display(Name = "Product Name")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Field 'Name' Mandatory")]
+        [StringLength(70, MinimumLength = 6, ErrorMessage = "Name field must contain at least 6 characters")]
         public string Name { get; set; }
 
         public string NameSlug => Name.ToLower().Replace(" ", "-");
 
         [Display(Name = "Description")]
-        public string Description { get; set; }
+		[Required(AllowEmptyStrings = false, ErrorMessage = "Field 'Description' Mandatory")]
+		[StringLength(175, MinimumLength = 25, ErrorMessage = "Description field must contain at least 25 characters")]
+		public string Description { get; set; }
+
         [Display(Name = "Image Path")]
-        public string ImageUri { get; set; }
+		[Required(AllowEmptyStrings = false, ErrorMessage = "Field 'Image' Mandatory")]
+		public string ImageUri { get; set; }
+
         [Display(Name = "Price")]
         [DataType(DataType.Currency)]
-        public double Price { get; set; }
+		[Required(AllowEmptyStrings = false, ErrorMessage = "Field 'Price' Mandatory")]
+		public double Price { get; set; }
+
         [Display(Name = "Express Delivery")]
-        public bool ExpressDelivery { get; set; }
+		public bool ExpressDelivery { get; set; }
         public string FormatExpressDelivery => ExpressDelivery ? "Yes" : "No";
 
         [Display(Name = "Available In")]
         [DisplayFormat(DataFormatString = "{0:MMMM yyyy}")]
-        public DateTime RegistrationDate { get; set; }
+		[Required(AllowEmptyStrings = false, ErrorMessage = "Field 'Available In' Mandatory")]
+		public DateTime RegistrationDate { get; set; }
     }
 }
---
+++ AraujoMaterialConstrucao/Pages/Create.cshtml.cs
@@ -0,0 +1,14 @@
+using AraujoMaterialConstrucao.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AraujoMaterialConstrucao.Pages
+{
+    public class InsertModel : PageModel
+    {
+        public Tile Tile { get; set; }
+        public void OnGet()
+        {
+        }
+    }
+}
