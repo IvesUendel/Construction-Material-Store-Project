5e325f4e1bb70b3f393dcf4a547c368e77ba02ea
Uendel Ives de Araujo <uendel.profissional@gmail.com>
2023-11-23 20:32:03 -0300

    Created the data folder and the class for directional connection to the Database and added the Table function in the Tile class to create the table with the directional name

---
+++ AraujoMaterialConstrucao/Data/MaterialConstructionDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AraujoMaterialConstrucao.Data;
+
+public class MaterialConstructionDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var config = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json").Build();
+
+        string conn = config.GetConnectionString("Conn");
+
+        optionsBuilder.UseSqlServer(conn);
+    }
+}
