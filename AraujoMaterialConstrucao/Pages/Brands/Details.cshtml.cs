e75c683060392a0538820a7b639aae65aaa09152
Uendel Ives de Araujo <uendel.profissional@gmail.com>
2023-12-08 22:21:40 -0300

    Created the CRUD for the Brands option, made some changes to the create page layout and added the Identity packages

---
+++ AraujoMaterialConstrucao/Pages/Brands/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using AraujoMaterialConstrucao.Data;
+using AraujoMaterialConstrucao.Models;
+
+namespace AraujoMaterialConstrucao.Pages.Brands
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly AraujoMaterialConstrucao.Data.MaterialConstructionDbContext _context;
+
+        public DetailsModel(AraujoMaterialConstrucao.Data.MaterialConstructionDbContext context)
+        {
+            _context = context;
+        }
+
+      public Brand Brand { get; set; } = default!; 
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Brand == null)
+            {
+                return NotFound();
+            }
+
+            var brand = await _context.Brand.FirstOrDefaultAsync(m => m.BrandId == id);
+            if (brand == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Brand = brand;
+            }
+            return Page();
+        }
+    }
+}
---
+++ AraujoMaterialConstrucao/Pages/Brands/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using AraujoMaterialConstrucao.Data;
+using AraujoMaterialConstrucao.Models;
+
+namespace AraujoMaterialConstrucao.Pages.Brands
+{
+    public class IndexModel : PageModel
+    {
+        private readonly AraujoMaterialConstrucao.Data.MaterialConstructionDbContext _context;
+
+        public IndexModel(AraujoMaterialConstrucao.Data.MaterialConstructionDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Brand> Brand { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Brand != null)
+            {
+                Brand = await _context.Brand.ToListAsync();
+            }
+        }
+    }
+}
